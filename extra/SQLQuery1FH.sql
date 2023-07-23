64722f502fff1243e93793d08d15ec80de703912
jojimenezsalas <jojimenezsalas@gmail.com>
2023-07-23 16:37:45 -0600

    db

--- extra/SQLQuery1FH.sql
+++ extra/SQLQuery1FH.sql
@@ -10,6 +10,37 @@ ApellidoPaterno varchar(100),
 ApellidoMaterno varchar(100),
 FechaIngreso date,
 );
+create procedure UpdateUsuario
+@IdUsuario int,@IdSupervisor int ,@Nombre varchar(20),@ApellidoPaterno varchar(20), @ApellidoMaterno varchar(20),
+@FechaIngreso date
+as
+begin
+UPDATE Usuario SET  IdSupervisor=@IdSupervisor,Nombre=@Nombre,ApellidoPaterno=@ApellidoPaterno,ApellidoMaterno=@ApellidoMaterno,FechaIngreso=@FechaIngreso where IdUsuario=@IdUsuario
+end;
+
+create procedure UsuarioAdd
+@IdSupervisor int, @Nombre varchar(20),@ApellidoPaterno varchar(20),@ApellidoMaterno varchar(20),@FechaIngreso date
+as
+begin
+	INSERT INTO Usuario (IdSupervisor,Nombre,ApellidoPaterno,ApellidoMaterno,FechaIngreso)values(@IdSupervisor,@Nombre,@ApellidoPaterno,@ApellidoMaterno,@FechaIngreso)
+end;
+CREATE PROCEDURE GetByIdUsuario
+@IdUsuario int
+as 
+begin
+select IdUsuario,IdSupervisor,Nombre,ApellidoPaterno,ApellidoMaterno,FechaIngreso from Usuario where IdUsuario=@IdUsuario
+end;
+
+
+create procedure GetSupervisor
+as
+begin
+select IdSupervisor,NombreSupervisor from Supervisor
+end;
+
+
+
+
 
 create table Area(
 IdArea int primary key identity(1,1),
@@ -26,11 +57,19 @@ create table Equipo(
 IdEquipo int primary key identity(1,1),
 Modelo varchar(100),
 IdMarca int references Marca(IdMarca),
-IdTipo int references Tipo(IdTipo),
 ClaveInventario varchar(100) unique,
-IdTipoEquipo int references TipoEquipoArea(IdTipoEquipo),
+IdTipoEquipo int references TipoEquipo(IdTipoEquipo),
 IdArea int references Area(IdArea)
 );
+
+
+
+create procedure GetEquipos
+as
+begin
+select Equipo.IdEquipo,Equipo.Modelo,ClaveInventario,TipoEquipo.IdTipoEquipo,TipoEquipo.NombreTipo from Equipo inner join TipoEquipo on TipoEquipo.IdTipoEquipo=Equipo.IdTipoEquipo
+
+end;
 
 
 create table Tipo(
@@ -45,7 +84,7 @@ NombreMarca varchar(100)
 
 create table TipoEquipo(
 IdTipoEquipo int primary key identity(1,1),
-NombreTipoArea varchar(100)
+NombreTipo varchar(100)
 );
 
 
@@ -57,26 +96,60 @@ IdEquipo int references Equipo(IdEquipo),
 FechaAsignada date
 );
 
+create procedure AddAsignar
+@IdEquipo int, @IdUsuario int
+as
+begin
+insert into Asignacion (IdEquipo,IdUsuario,FechaAsignada)values(@IdEquipo,@IdUsuario,GETDATE())
+end
+
+CREATE PROCEDURE GetAllUsuario
+as 
+begin
+select IdUsuario,IdSupervisor,Nombre,ApellidoPaterno,ApellidoMaterno,FechaIngreso from Usuario
+end;
+
 
 Create procedure GetAllAsignaciones
 As
 begin
-select IdAsignacion,Usuario.IdUsuario, Equipo.IdEquipo, Usuario.Nombre,Equipo.IdMarca,Equipo.Modelo,Asignacion.FechaAsignada from Asignacion inner join Usuario on Asignacion.IdUsuario= Usuario.IdUsuario
+select IdAsignacion,Usuario.IdUsuario, Equipo.IdEquipo, Usuario.Nombre,Area.IdArea,Area.Descripccion,Marca.IdMarca,Marca.NombreMarca,Equipo.IdMarca,Equipo.Modelo,Asignacion.FechaAsignada from Asignacion inner join Usuario on Asignacion.IdUsuario= Usuario.IdUsuario
 inner join Equipo on Equipo.IdEquipo = Asignacion.IdEquipo
+inner join Area on Equipo.IdArea=Area.IdArea
+inner join Marca on Equipo.IdMarca=Marca.IdMarca
+
 end;
+
+
+
+SELECT * FROM Equipo
+SELECT * FROM Asignacion
+
+select * from Area
+
+truncate table Asignacion
+
+
+
+
+
+
+
 
 CREATE PROCEDURE EquiposSinAsignar
 AS
 BEGIN
-    SELECT e.IdEquipo, e.Modelo, e.ClaveInventario
-    FROM Equipo e
-    LEFT JOIN Asignacion a ON e.IdEquipo = a.IdEquipo
-    WHERE a.IdEquipo IS NULL;
+    SELECT Equipo.IdEquipo,Equipo.Modelo,Equipo.ClaveInventario
+    FROM Equipo 
+    LEFT JOIN Asignacion  ON Equipo.IdEquipo = Asignacion.IdEquipo
+    WHERE Asignacion.IdEquipo IS NULL;
 END;
 
+select * from Equipo
 
-SELECT * FROM Asignacion
-
-
-
-
+create procedure DeleteAsignacion
+@IdAsignacion int
+as
+begin
+DELETE FROM Asignacion where IdAsignacion=@IdAsignacion
+end
